652cc2bd4bad1384c28351ad877ad26378903b04
Ifty Anwar <153787843+therealifty@users.noreply.github.com>
2025-04-28 09:27:47 +0600

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+// firebase-config.js
+var firebaseConfig = {
+    apiKey: "AIzaSyAEEurR0FMrMt_2HwfTR6C2Db6KDvmfTaE",
+    authDomain: "vault-2c2cd.firebaseapp.com",
+    projectId: "vault-2c2cd",
+    storageBucket: "vault-2c2cd.firebaseapp.com",
+    messagingSenderId: "911466241989",
+    appId: "1:911466241989:web:2736572e05c4abcd7135ac",
+    measurementId: "G-FYVRG02FE1"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
